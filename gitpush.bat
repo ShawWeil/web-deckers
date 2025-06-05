e6253fea45a83c86e56e78bf332e6bf326a14fea
rweilwww <github@shaw-weil.com>
2025-06-05 10:22:08 -0400

    from indesign 2025-06-05 14:21

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/deckers
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/deckers
-   git remote add origin https://github.com/ShawWeil/web-deckers.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from indesign 2025-06-05 14:06" --no-edit
+git commit -m "from indesign 2025-06-05 14:21" --no-edit
 git push --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
